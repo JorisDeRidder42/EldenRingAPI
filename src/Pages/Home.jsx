d54ad58bc64a31aba12adbfe796134b9e111da10
jorisderidder42 <jorisderidder@outlook.be>
2024-01-09 18:09:38 +0100

    feature: parallax scroll

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -5,15 +5,12 @@ import { Parallax } from 'react-parallax';
 const Home = () => {
   return(
       <>
-        <Parallax blur={0} bgImage="../assets/parallax/full.png" bgImageAlt="background" strength={-200}>
+        <Parallax blur={0} bgImage="../assets/parallax/full.png" bgImageAlt="background" strength={1}>
           <div className='content'>
             <span className='img-txt'>JOURNEY THROUGH THE LANDS BETWEEN</span>
             <img src="../assets/parallax/hero.png" alt='hero' className='hero'/>
           </div>
         </Parallax>
-        <div className='bg-dark h-screen'>
-         
-        </div>
       </>
   )
 }
--- src/Pages/PageNotFound.jsx
+++ src/Pages/PageNotFound.jsx
@@ -4,6 +4,7 @@ import { Link } from "react-router-dom";
 const PageNotFound = () => {
     return(
         <div className="page404">
+                <img src="../assets/parallax/404.jpg" alt='dragon and knight' />
           <div className="middle">
                 <h1>404</h1>
                 <h6>Did not found the page</h6>
