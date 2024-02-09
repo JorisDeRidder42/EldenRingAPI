e0c4df07f2280f4ee2680e9efa8902d67c0939df
jorisderidder42 <jorisderidder@outlook.be>
2024-02-09 22:16:28 +0100

    added variable css to style.css

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,13 +1,18 @@
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import { Image } from 'react-bootstrap';
+import elden from '../assets/404.jpg'
 const Home = () => {
   return(
       <>
       <Container>
         <Row>
-          <Col xs={6}>
-            <h1>The Golden Order has been broken.</h1>
+          <Col  md={4}>
+            <Image src={elden} alt={'Dragon from elden ring'}/>
+          </Col>
+          <Col md={8}>
+              <h2 className='text-end mt-5 cta-text'>The Golden Order has been broken.</h2>
           </Col>
         </Row>
       </Container>
