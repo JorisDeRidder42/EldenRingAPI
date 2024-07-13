a4c7e228e200d720722802420be8145fa0e1f713
jorisderidder42 <jorisderidder@outlook.be>
2024-07-14 00:04:12 +0200

    feature: parallax home screen

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,37 +1,21 @@
-import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
+import {Container} from 'react-bootstrap';
 import { Image } from 'react-bootstrap';
-import elden from '../assets/404.jpg'
+import hero from '../assets/parallax/charachter.png';
 import LanguageContext from '../Context/languageContext';
 import useLanguage from '../Hooks/useLanguage';
 import { useContext } from 'react';
+import { Parallax } from 'react-scroll-parallax';
 
 const Home = () => {
   const {text} = useLanguage()
     const {selectedLanguage} = useContext(LanguageContext)
   return(
       <>
-      <Container >
-        <Row>
-          <Col  md={4}>
-            <Image src={elden} alt={'Knight fighting a dragon'}/>
-          </Col>
-          <Col md={8}>
-              <h2 className='text-end mt-5 cta-text'>{text['intro']}</h2>
-          </Col>
-        </Row>
-      </Container>
-      <Container>
-        <Row>
-          <Col  md={4}>
-            <h2 className='text-end mt-5 cta-text'>{text['eldenLord']}</h2>
-          </Col>
-          <Col md={8}>
-              <h2 className='text-end mt-5 cta-text'>{text['intro']}</h2>
-          </Col>
-        </Row>
-      </Container>
+      <div className='bg-img'>
+          {/* <Parallax speed={10}> */}
+            <Image src={hero} className='hero' alt={'background'}/>
+          {/* </Parallax> */}
+      </div>
       </>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,7 @@ import {BrowserRouter} from 'react-router-dom';
 import "./style.css";
 import App from './App';
 import { AuthProvider } from './Context/authContext';
+import { ParallaxProvider } from 'react-scroll-parallax';
 
 
 
@@ -12,7 +13,9 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
     <AuthProvider>
+      <ParallaxProvider>
       <App/>
+      </ParallaxProvider>
     </AuthProvider>
     </BrowserRouter>
   </React.StrictMode>
