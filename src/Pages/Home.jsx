6add4086261f78a99a0988a7f13c04be89123cd2
jorisderidder42 <jorisderidder@outlook.be>
2024-07-17 23:03:22 +0200

    feature: parallax screen on homepage

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,26 +1,29 @@
-import {Container} from 'react-bootstrap';
-import { Image } from 'react-bootstrap';
-import hero from '../assets/parallax/hero.png';
+
+import hero from '../assets/parallax/charachter.png';
 import bg from '../assets/parallax/Bg-parallax.jpg';
 import LanguageContext from '../Context/languageContext';
 import useLanguage from '../Hooks/useLanguage';
 import { useContext } from 'react';
 import { Parallax } from 'react-parallax';
 import HomeAbout from './HomeAbout';
+import { MdKeyboardDoubleArrowDown } from "react-icons/md";
 
 const Home = () => {
   const {text} = useLanguage()
     const {selectedLanguage} = useContext(LanguageContext)
   return(
-      <div className="bg-img">
-      <Parallax className="bg-img" strength={200} blur={1} bgImage={bg} bgImageAlt="the bg">
+      <div className='bg-img'>
+      <Parallax blur={2} bgImage={bg} strength={-200}>
         <div className='center'>
-          <h1 className='text-white title'>The Golden Order has been broken.</h1>
-          <img src={hero} alt='hero' className='hero'/>
+        <h1 className='text-white title'>The Golden Order has been broken.</h1>
+        <img src={hero} className='hero' alt="hero" />
+        </div>
+        <div className='icon'>
+          <p>Scroll <MdKeyboardDoubleArrowDown /></p>
         </div>
       </Parallax>
       <HomeAbout/>
-      </div>
-  )
+    </div>
+  ) 
 }
 export default Home;
